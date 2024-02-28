e6d1cc08f246160cef75ef267cbe9d4a6f0cfd2a
Amao-J <fredjonathan22@yahoo.com>
2024-02-28 23:13:31 +0100

    Update settings.py

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -22,12 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9e97t5r5i^#lj493mgqyeu@#yjp^w-&ou@u--r0&z5jxx1y712'
+# SECRET_KEY = 'django-insecure-9e97t5r5i^#lj493mgqyeu@#yjp^w-&ou@u--r0&z5jxx1y712'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-9e97t5r5i^#lj493mgqyeu@#yjp^w-&ou@u--r0&z5jxx1y712')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
