6f9af7b63482613fced2141bf3b78bcb926c07cc
Amao-J <fredjonathan22@yahoo.com>
2024-02-29 10:00:11 +0100

    Update settings.py

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -153,7 +153,7 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'mediafiles'
 
 
-INTERNAL_IPS = ['127.0.0.1']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
